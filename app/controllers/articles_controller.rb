c06c79bed6337b9d33dd455da3de8351c3f534a8
nudelx <alex.nudelman@samanage.com>
2015-12-08 17:23:01 +0200

    Extract common functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :get_article , only: [:edit, :update, :show, :destroy]
+
 	def index
 		@articles = Article.all
   end
@@ -20,15 +22,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def edit
-    @article = Article.find(params[:id])
+   
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if(@article.update(article_params))
       flash[:notice] = "Article was updated !!! "
       redirect_to article_path(@article)
@@ -40,7 +42,7 @@ class ArticlesController < ApplicationController
 
   def destroy
 
-  	@article = Article.find(params[:id])  	
+  	
   	@article.destroy
   	flash[:notice] = 'Article was deleted'
   	redirect_to articles_path
@@ -50,6 +52,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def get_article
+    @article = Article.find(params[:id])
   end
 
 
