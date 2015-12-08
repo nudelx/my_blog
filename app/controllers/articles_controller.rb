064eeac0056a5146d4b956d4094b46d4e60bb0de
nudelx <alex.nudelman@samanage.com>
2015-12-08 15:46:57 +0200

    new links where aded

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,48 @@
 class ArticlesController < ApplicationController
-	
-	def new
-		@article = Article.new()
-	end
 
-	def create
-		@article = Article.new(article_params)
+	def index
+		@articles = Article.all
+  end
 
-		if @article.save
-			flash[:notice] = "Article was created !!! "
-			redirect_to article_path(@article)
-		else
-			render :new
-		end
-	end
+  def new
+    @article = Article.new()
+  end
 
-	def show
-		@article = Article.find(params[:id])
-		
-	end
+  def create
+    @article = Article.new(article_params)
 
-	private
+    if @article.save
+      flash[:notice] = "Article was created !!! "
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
 
-	def article_params
-		params.require(:article).permit(:title, :description)
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-	
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if(@article.update(article_params))
+      flash[:notice] = "Article was updated !!! "
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+
 end
