1389010fb8177aa96062ad34cafaeb7ab27082c9
nudelx <alex.nudelman@samanage.com>
2015-12-08 14:40:17 +0200

    added option to create new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
 
-	validates :title ,presence: true, length: {minimum: 3, maximum: 50, message:"fff"}
+	validates :title ,presence: true 
+	validates :description ,presence: true, length:{ minimum: 3, maximum:10}
 
-	validates :description ,presence: true, length: {minimum: 10, maximum: 300 }
+	   
 
 end
