eb620a8dd98774599165981bf9369d97615f64c5
nudelx <alex.nudelman@samanage.com>
2015-12-09 14:44:29 +0200

    done style for main page , listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 
 	validates :title ,presence: true 
-	validates :description ,presence: true, length:{ minimum: 3, maximum:10}
+	validates :description ,presence: true, length:{ minimum: 3}
 
 	   
 
