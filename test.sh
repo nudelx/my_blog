3d33f71d06bfde26469a87ac964d650dff0282ef
Alex Nudelman <nudelx@gmail.com>
2015-12-09 23:17:09 +0200

    new model users

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+
+DIRECTORY='.git'
+echo \[\033[32m\] sss
+
+if [ -d "./$DIRECTORY" ]; then
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+else
+ echo "NOOO !!"
+fi
+
+
+
+
+
