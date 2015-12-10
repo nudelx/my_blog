b404927e931e0ff7e13a38306ac460b2fc5d79cb
nudelx <alex.nudelman@samanage.com>
2015-12-10 13:53:33 +0200

    Added user meta data

--- test.sh
+++ test.sh
@@ -5,8 +5,7 @@ DIRECTORY='.git'
 echo \[\033[32m\] sss
 
 if [ -d "./$DIRECTORY" ]; then
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-else
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'s
 fi
 
 
