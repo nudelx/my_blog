8253ec5c162efffa63209568e71ebd8abf360888
Alex Nudelman <nudelx@gmail.com>
2015-12-14 00:24:02 +0200

    added users list and new view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,22 @@
 class ArticlesController < ApplicationController
+  before_action :get_article, only: [:edit, :update, :show, :destroy]
 
-  before_action :get_article , only: [:edit, :update, :show, :destroy]
-
-	def index
-		@articles = Article.all
+  def index
+    @articles = Article.all
   end
 
   def new
-    @article = Article.new()
+    @article = Article.new
   end
 
   def create
     # debugger
     # render plain: params.inspect
     @article = Article.new(article_params)
-    @article.user  = User.first
-
+    @article.user = User.first
 
     if @article.save
-      flash[:success] = "Article was created !!! "
+      flash[:success] = 'Article was created !!! '
       redirect_to article_path(@article)
     else
       render :new
@@ -26,30 +24,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
-   
   end
 
   def update
-    
-    if(@article.update(article_params))
-      flash[:success] = "Article was updated !!! "
+    if @article.update(article_params)
+      flash[:success] = 'Article was updated !!! '
       redirect_to article_path(@article)
     else
       render :edit
     end
-
   end
 
   def destroy
-
-  	
-  	@article.destroy
-  	flash[:danger] = 'Article was deleted'
-  	redirect_to articles_path
+    @article.destroy
+    flash[:danger] = 'Article was deleted'
+    redirect_to articles_path
   end
 
   private
@@ -61,6 +53,4 @@ class ArticlesController < ApplicationController
   def get_article
     @article = Article.find(params[:id])
   end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,44 @@
 class UsersController < ApplicationController
-	
-	def new
-		@user  = User.new
-	end
 
+def index
+	@users = User.all
+end
 
-	def create
-		@user = User.new(get_user_params)
-		if(@user.save)
-			flash[:success] = "User was created !!! "
-			redirect_to articles_path
-		else
-			render 'new'
-		end
-	end
+  def new
+    @user = User.new
+  end
 
-	def edit
-		@user =  User.find(params[:id])
-	end
+  def create
+    @user = User.new(get_user_params)
+    if @user.save
+      flash[:success] = 'User was created !!! '
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@user =  User.find(params[:id])
-		if(@user.update(get_user_params))
-			flash[:success] = "User was updated !!! "
-			redirect_to articles_path
-		else
-			render 'edit'
-		end
-	end
-	
-	def show
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	private 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(get_user_params)
+      flash[:success] = 'User was updated !!! '
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
 
-	def get_user_params
+  def show
+    @user = User.find(params[:id])
+  end
 
-		params.require(:user).permit(:username, :email, :password)
-		
-	end
+  private
+
+  def get_user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
