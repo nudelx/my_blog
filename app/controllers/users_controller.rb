736871f9f0b8fe866fb84c10c09263011fc21163
Alex Nudelman <nudelx@gmail.com>
2015-12-13 21:49:35 +0200

    signup done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,27 @@ class UsersController < ApplicationController
 	def new
 		@user  = User.new
 	end
+
+
+	def create
+		@user = User.new(get_user_params)
+		if(@user.save)
+			flash[:success] = "User was created !!! "
+			redirect_to articles_path
+		else
+			render 'new'
+		end
 	
+
+	end
+	
+
+
+	private 
+
+	def get_user_params
+
+		params.require(:user).permit(:username, :email, :password)
+		
+	end
 end
